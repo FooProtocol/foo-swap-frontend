21554ada85976617b0bb2fc578a2836bac697194
Developer Uche <nweleuchenna2004@gmail.com>
2023-02-06 01:34:50 +0100

    developeruche: done witht the frontend interaction fixes

--- src/constants/multicall/index.ts
+++ src/constants/multicall/index.ts
@@ -2,8 +2,8 @@ import { ChainId } from '@fooswap-libs/sdk'
 import MULTICALL_ABI from './abi.json'
 
 const MULTICALL_NETWORKS: { [chainId in ChainId]: string } = {
-  [ChainId.MAINNET]: '0x1Ee38d535d541c55C9dae27B12edf090C608E6Fb', // TODO
-  [ChainId.BSCTESTNET]: '0x301907b5835a2d723Fe3e9E8C5Bc5375d5c1236A',
+  [ChainId.MAINNET]: '0x23520e4e6a01387da9a56737618125CD0F97dE28', // TODO
+  [ChainId.BSCTESTNET]: '0x23520e4e6a01387da9a56737618125CD0F97dE28',
 }
 
 export { MULTICALL_ABI, MULTICALL_NETWORKS }
